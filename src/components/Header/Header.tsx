0654a7eab1c11e68a93b4e3a7ab657d46c640992
Choi_mingi <mingi12304@sevensystem.net>
2024-12-23 10:44:32 +0900

    meta 롤백, header padding 수정

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import Logo from "@/assets/image/logo.svg";
 
 const Header = () => {
   return (
-    <header className="flex w-full items-center justify-between h-[128px] p-4 bg-mainColor text-white">
+    <header className="flex w-full items-center justify-between h-[128px] px-[3.2rem] py-[1.6rem] bg-mainColor text-white">
       <img src={Logo} alt="Logo" className="w-[96px] h-[96px] mr-2" />
       <span className="text-fz1 pr-10">필동면옥</span>
       <span className="text-fz1">09:16:32</span>
